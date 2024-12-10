99deba37650d6e9090910e7a5c205bbdf739d7c8
94adi <adrianrcotuna@gmail.com>
2024-12-10 22:46:02 +0200

    Added WebApp with scaffold identity

---
+++ EDAS/EDAS.WebApp/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EDAS.WebApp.Data;
+using EDAS.WebApp.Areas.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("EDASWebAppContextConnection") 
+    ?? throw new InvalidOperationException("Connection string 'EDASWebAppContextConnection' not found.");
+
+builder.Services.AddDbContext<EDASWebAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<EDASWebAppUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true;
+})
+.AddEntityFrameworkStores<EDASWebAppContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
